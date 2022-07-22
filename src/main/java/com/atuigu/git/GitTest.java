d8532b389b96ac83ce00209d3fc585f4f229f4d2
Ouyang <932434088@qq.com>
2022-07-22 14:21:28 +0800

    hot-fix first commit

--- src/main/java/com/atuigu/git/GitTest.java
+++ src/main/java/com/atuigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello ouyang!");
         System.out.println("hello ouyang3!");
+        System.out.println("hello ouyang4!");
     }
 }
