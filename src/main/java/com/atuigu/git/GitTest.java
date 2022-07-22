b558ccbf0021631d836afaa7bd40c66d4fb92e53
Ouyang <932434088@qq.com>
2022-07-22 14:25:58 +0800

    master commit1

--- src/main/java/com/atuigu/git/GitTest.java
+++ src/main/java/com/atuigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello ouyang!");
         System.out.println("hello ouyang3!");
         System.out.println("hello ouyang4!");
+        System.out.println("master!");
     }
 }
